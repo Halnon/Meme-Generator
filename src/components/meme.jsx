47e1235222eb641a017ff16f23330ec590341ad9
Adam Halnon <halnon_01@hotmail.com>
2024-05-17 15:37:55 +0100

    redesign component to make it more readable and simple

--- src/components/meme.jsx
+++ src/components/meme.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React from "react"
 
 export default function Meme() {
     return (
-        <form className="form--container">
-            <div className="labels--container">
-                <div id="label--container1">
-                    <p className="label--title">Top text</p>
-                    <label>
-                        <input placeholder="Shut up" />
-                    </label>
-                </div>
-                <div id="label--container2">
-                    <p className="label--title">Bottom text</p>
-                    <label>
-                        <input placeholder="and take my money" />
-                    </label>
-                </div>
-
-            </div>
-            <input type="submit" value="Get a new meme image" className="submit--input" />
-        </form>
+        <main>
+            <form className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                >
+                    Get a new meme image 🖼
+                </button>
+            </form>
+        </main>
     )
 }
