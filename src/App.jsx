dbc06c9dc87c806e217e5c1d636f709d542001f5
Adam Halnon <halnon_01@hotmail.com>
2024-05-17 14:45:41 +0100

    import and render meme component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Header from "./components/header.jsx"
+import Meme from "./components/meme.jsx"
 
 export default function App() {
     return (
         <>
             <Header/>
+            <Meme/>
         </>
     )
 }
