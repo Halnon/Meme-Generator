da8de84c3bdf919d0500be724efc9a516c8fcd23
Adam Halnon <halnon_01@hotmail.com>
2024-05-17 14:58:14 +0100

    add extra divs to individual labels

--- src/components/meme.jsx
+++ src/components/meme.jsx
@@ -4,14 +4,19 @@ export default function Meme() {
     return (
         <form className="form--container">
             <div className="labels--container">
-                <p className="label--title">Top text</p>
-                <label>
-                    <input placeholder="Shut up" />
-                </label>
-                <p className="label--title">Bottom text</p>
-                <label>
-                    <input placeholder="and take my money" />
-                </label>
+                <div id="label--container1">
+                    <p className="label--title">Top text</p>
+                    <label>
+                        <input placeholder="Shut up" />
+                    </label>
+                </div>
+                <div id="label--container2">
+                    <p className="label--title">Bottom text</p>
+                    <label>
+                        <input placeholder="and take my money" />
+                    </label>
+                </div>
+
             </div>
             <input type="submit" value="Get a new meme image" className="submit--input" />
         </form>
